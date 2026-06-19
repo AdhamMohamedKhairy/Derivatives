2e0cc01f8d69791ffa1e7bf4b92b5ca668e0aefe
AdhamMohamedKhairy <adhamfaied14@gmail.com>
2026-06-19 20:41:35 +0300

    Completed most of l2 and l1 and added l3

--- l2.js
+++ l2.js
@@ -56,3 +56,20 @@ function drawChart() {
     MathJax.typesetPromise();
   }
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  const form1 = document.getElementById('quiz-form-1')
+  form1.addEventListener('submit', (e) => {
+        e.preventDefault()
+        const answer1 = parseFloat(document.getElementById('q1').value)
+        const result1 = document.getElementById('result-1')
+
+        if (answer1 === 4) {
+            result1.textContent = "Correct!";
+            result1.className = "result-message correct";
+        } else {
+            result1.textContent = "Wrong. Try again";
+            result1.className = "result-message wrong";
+        }
+      })
+});
